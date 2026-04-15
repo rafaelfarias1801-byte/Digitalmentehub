aa81fd7440a24056cc5fcf7225d4438004729890
rafaelfarias1801-byte <rafaelfarias1801@gmail.com>
2026-04-15 18:42:54 -0300

    fix: adiciona x-client-info e apikey nos headers CORS da tiktok-oauth

--- client/supabase/functions/tiktok-oauth/index.ts
+++ client/supabase/functions/tiktok-oauth/index.ts
@@ -10,7 +10,8 @@ const SCOPE         = "user.info.basic,video.upload";
 
 const CORS = {
   "Access-Control-Allow-Origin":  "*",
-  "Access-Control-Allow-Headers": "authorization, content-type",
+  "Access-Control-Allow-Headers": "authorization, content-type, x-client-info, apikey",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
 
 Deno.serve(async (req) => {
